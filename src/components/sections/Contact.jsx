e5048858fb9449145562502ffc074a6d397d8bcc
Cem <bcemcankaya@gmail.com>
2024-09-09 17:26:59 +0300

    formatting contact file

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -5,7 +5,9 @@ const Contact = () => {
     <section className="section-spacing">
       <div className="w-full rounded-md flex-col sm:flex-row flex gap-8 items-center justify-between px-4 py-8 bg-gradient-to-r from-fuchsia-600 to-purple-500">
         <div>
-          <h1 className="text-xl md:text-3xl font-bold">Get in Touch With Us</h1>
+          <h1 className="text-xl md:text-3xl font-bold">
+            Get in Touch With Us
+          </h1>
           <p className="text-md md:text-lg text-neutral-200">
             Have questions or need help? Fill out the form, and we will get back
             to you soon.
