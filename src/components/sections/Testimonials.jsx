feb9c7168b56d975add99c567ced3ec29fd8bb69
Cem <bcemcankaya@gmail.com>
2024-09-09 15:08:59 +0300

    Testimonial section template

--- src/components/sections/Testimonials.jsx
+++ src/components/sections/Testimonials.jsx
@@ -1,5 +1,24 @@
+import SectionHeader from "../common/SectionHeader";
+import { sections, testimonials } from "../../constants";
+import TestimonialCard from "../common/TestimonialCard";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const testimonialsData = sections.find(
+    (section) => section.type === "testimonials"
+  );
+
+  return (
+    <section className="section-spacing">
+      <div className="flex flex-col items-center justify-start md:justify-center">
+        <SectionHeader title={testimonialsData.title} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-8">
+        {testimonials.map((testimonial) => (
+          <TestimonialCard key={testimonial.id} {...testimonial} />
+        ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
