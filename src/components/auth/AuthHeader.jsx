20bf2452b82d58081913848e8d0f709c83c867a5
Cem <bcemcankaya@gmail.com>
2024-09-09 19:32:33 +0300

    moving AuthHeader component to auth folder

---
+++ src/components/auth/AuthHeader.jsx
@@ -0,0 +1,7 @@
+const AuthHeader = ({ children }) => {
+  return (
+    <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl max-w-full sm:max-w-3xl lg:max-5xl mx-auto font-bold">{children}</h1>
+  );
+};
+
+export default AuthHeader;
--- src/components/common/AuthHeader.jsx
+++
@@ -1,7 +0,0 @@
-const AuthHeader = ({ children }) => {
-  return (
-    <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl">{children}</h1>
-  );
-};
-
-export default AuthHeader;
