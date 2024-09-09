459bc821ed34a4f7d9ecacbff3254ad5e4e74d4e
Cem <bcemcankaya@gmail.com>
2024-09-09 19:32:49 +0300

    adding sign in header

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,16 @@
+import AuthHeader from "../components/auth/AuthHeader";
+
 const SignIn = () => {
-  return <div></div>;
+  return (
+    <section className="max-w-7xl mx-auto mt-10 px-4">
+      <div className="text-center">
+        <AuthHeader>
+          Welcome Back. We're here to simplify your journey in entrepreneurship.
+        </AuthHeader>
+      </div>
+      <form></form>
+    </section>
+  );
 };
 
 export default SignIn;
