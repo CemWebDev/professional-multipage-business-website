09918384892fba8c7cae42a6a25d638d68e3ceb2
Cem <bcemcankaya@gmail.com>
2024-09-09 18:05:54 +0300

    finished footer

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,5 +1,58 @@
+import { Link } from "react-router-dom";
+import { resourcesLinks, platformLinks, communityLinks } from "../../constants";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className="max-w-[1100px] xl:max-w-full py-4 px-8">
+      <div className="flex items-center sm:items-start flex-wrap gap-8 justify-between mt-10 border-t border-neutral-500 py-4">
+        <div className="text-3xl uppercase font-bold tracking-wider text-fuchsia-600">
+          <Link>Logo</Link>
+        </div>
+        <div className="flex flex-col items-start gap-4">
+          <h1>Resources</h1>
+          <ul className="flex flex-col items-start gap-2">
+            {resourcesLinks.map((link, index) => (
+              <Link
+                key={index}
+                to={link.href}
+                className="hover:text-fuchsia-500"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className="flex flex-col items-start gap-4">
+          <h1>Platforms</h1>
+          <ul className="flex flex-col items-start gap-2">
+            {platformLinks.map((link, index) => (
+              <Link
+                key={index}
+                to={link.href}
+                className="hover:text-fuchsia-500"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className="flex flex-col items-start gap-4">
+          <h1>Community</h1>
+          <ul className="flex flex-col items-start gap-2">
+            {communityLinks.map((link, index) => (
+              <Link
+                key={index}
+                to={link.href}
+                className="hover:text-fuchsia-500"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
